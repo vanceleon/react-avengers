596c6091cc2ac18a1ad235fd928a766e8e964302
vanceleon <vanceleon44@gmail.com>
2018-08-06 13:57:03 -0400

    new dir for avengers repo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      avengers: ['Iron-man','Black-Widow', 'Captain America', 'Hulk', 'Thor','Vision'],
+    }
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,10 +18,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Avengers: by Vance Leon</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        {this.state.avengers.map(avenger => <div key={avenger}>{avenger}</div>)}
+        </div>
     );
   }
 }
